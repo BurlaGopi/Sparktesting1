aa76e2c7822c3a46b273b52aabb0c3ce5be4b9ee
Gopii@123 <burla.gopi38@gmail.com>
2024-08-13 20:48:07 +0530

    created sparksession

---
+++ src/main/scala/newproject.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.sql.SparkSession
+
+object newproject {
+
+  def main(arrgs:Array[String]):Unit={
+    val v=39
+    println(v)
+
+    val spark=SparkSession.builder().appName("newapp").master("local").getOrCreate()
+
+    val df=spark.read.format("csv").option("header","true").option("delimiter",",").load("C:\\Users\\HP\\Desktop\\new_file.txt")
+  df.show()
+  }
+}
